be3750d63d0c90ba41355453833f1dbd64e88f68
Cassandra <cassandralelei013@gmail.com>
2023-06-19 00:07:40 -0700

    env.c and env1.c files added

---
+++ env.c
@@ -0,0 +1,38 @@
+#include "shell"
+
+/**
+ * envCopy - a function that is used to create a
+ * copy of the environment variables.
+ * Return: NULL incase of an error or
+ * O/w, a double pointer to the new copy.
+ */
+char **envCopy(void)
+{
+	char **newEnvn;
+	size_t size;
+	int index;
+
+	for (size = 0; environ[size]; size++)
+		;
+
+	newEnvn = malloc(sizeof(char *) * (size + 1));
+	if (!newEnvn)
+		return (NULL);
+
+	for (index = 0; environ[index]; index++)
+	{
+		newEnvn[index] = malloc(_strlen(environ[index]) + 1);
+
+		if (!newEnvn[index])
+		{
+			for (index--; index >= 0; index--)
+				free(newEnvn[index]);
+			free(newEnvn);
+			return (NULL);
+		}
+		_strcpy(newEnvn[index], environ[index]);
+	}
+	newEnvn[index] = NULL;
+
+	return (newEnvn);
+}
---
+++ env1.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * envFree - a function that is used to free
+ * the copied environment variable.
+ * Return: nothing
+ */
+void envFree(void)
+{
+	int index;
+
+	for (index = 0; environ[index]; index++)
+		free(environ[index]);
+	free(environ);
+}
+
+/**
+ * locate_env - a function used to get an
+ * environmental variable from the PATH.
+ * @var: The environmental variable to get.
+ * Return: NULL, If the environmental variable is absent
+ * Otherwise, a pointer to the environmental variable.
+ */
+char **locate_env(const char *var)
+{
+	int index;
+	int length;
+
+	length = _strlength(var);
+	for (index = 0; environ[index]; index++)
+	{
+		if (_strncmp(var, environ[index], length) == 0)
+			return (&environ[index]);
+	}
+
+	return (NULL);
+}
