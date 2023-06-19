fe2e502fd54b5e1bb0e6211adf9ee7861e2a5bca
B2ma <beatrice.terer@gmail.com>
2023-06-19 09:48:41 +0000

    Created separate_tokens.c

---
+++ separate_tokens.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+  * tokenLength - finds the delimiter index
+  * @str: the string contsining delimiters
+  * @delim: delimiter character
+  * Return: length of string before delimiter
+  */
+int tokenLength(char *str, char *delim)
+{
+	int itr = 0 length = 0;
+
+	while (*(str + itr) && *(str + itr) != *delim)
+	{
+		length++;
+		itr++;
+	}
+	return (length);
+}
+/**
+  * tokenCount - counts number of words separated by delimiters
+  * @str: string to find tokens
+  * @delim: the delimiter character
+  * Return: number of words in str
+  */
+int tokenCount(char *str, char *delim)
+{
+	int itr, words = 0, length = 0;
+
+	for (itr = 0; *(str + itr); itr++)
+		length++;
+	for (itr = 0; itr < length; itr++)
+	{
+		if (*(str + itr) != *delim)
+		{
+			words++;
+			itr += tokenLength(str + itr, delim);
+		}
+	}
+	return (words);
+}
+/**
+  * strtok_fn - Tokenizes str
+  * @str: string to tokenize
+  * @delim: delimiters
+  * Return: pointer to array of words
+  */
+char **strtok_fn(char *str, char *delim)
+{
+	char **words;
+	int itr = 0;
+	int the_words, a, characters, b;
+
+	the_words = tokenCount(str, delim);
+	if (the_words == 0)
+		return (NULL);
+	words = malloc(sizeof(char *) * (the_words + 2));
+	if (!words)
+		return (NULL);
+	for (a = 0; a < the_words; a++)
+	{
+		while (str[itr] == *delim)
+			itr++;
+		characters = tokenLength(str + itr, delim);
+		words[a] = malloc(sizeof(char) * (characters + 1));
+		if (!words[a])
+		{
+			for (itr -= 1; itr >= 0; itr--)
+				free(words[itr]);
+			free(words);
+			return (NULL);
+		}
+		for (b = 0; b < words; b++)
+		{
+			words[a][b] = str[itr];
+			itr++;
+		}
+		words[a][b] = '\0';
+	}
+	words[a] = NULL;
+	words[a + 1] = NULL;
+	return (words);
+}
+
+
